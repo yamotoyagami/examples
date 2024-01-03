f7b36e72926ca39a302b0a18c9ff12096a99224a
yamotoyagami <149977527+yamotoyagami@users.noreply.github.com>
2024-01-03 11:45:27 +0100

    Update erc1155.sol

--- erc1155.sol
+++ erc1155.sol
@@ -1,1 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MyToken is ERC1155 {
+    constructor() ERC1155("") {}
+}
