46c5d1449dadae891fd83d813251213e4c664e65
yamotoyagami <149977527+yamotoyagami@users.noreply.github.com>
2024-03-13 12:10:54 +0100

    Update pepe.sol

--- pepe.sol
+++ pepe.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract Pepe is ERC20, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("Pepe", "PP")
+        Ownable(initialOwner)
+        ERC20Permit("Pepe")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
